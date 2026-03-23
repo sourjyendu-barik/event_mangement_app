e6aa8ad4701a388a093144f777404d14a6bd75da
sourjyendu-barik <sourjyendubarik7798@gmail.com>
2026-03-23 16:15:32 +0530

    added hosted api

--- src/api/AxiosInstance.js
+++ src/api/AxiosInstance.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 export const AxiosInstance = axios.create({
-  baseURL: "http://localhost:5000/",
+  // baseURL: "http://localhost:5000/",
+  baseURL: "https://event-mangement-six.vercel.app/",
   timeout: 10000,
   withCredentials: true, // This enables sending/receiving cookies
   headers: { "Content-Type": "application/json" },
